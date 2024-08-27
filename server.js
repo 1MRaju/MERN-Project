54eab77f2386d6b05373b70afee99e709bf84990
raju <rajuchavan14599@gmail.com>
2024-08-27 14:13:18 +0530

    updated

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use('/api/v1/inventory', require('./routes/inventoryRoutes'));
 app.use('/api/v1/analytics', require('./routes/analyticsRoutes'));
 app.use('/api/v1/admin', require('./routes/adminRoutes'));
 
+
 //static files
 app.use(express.static(path.join(__dirname, "./blood-bank/build")));
 
